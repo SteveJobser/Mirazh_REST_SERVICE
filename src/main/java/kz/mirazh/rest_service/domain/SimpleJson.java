b210bb6600175511543271b62ceb5d860cd6c69e
Temerlan <tima_tleg@mail.ru>
2023-07-28 23:52:00 +0600

    MZ-2 || add constructor

--- src/main/java/kz/mirazh/rest_service/domain/SimpleJson.java
+++ src/main/java/kz/mirazh/rest_service/domain/SimpleJson.java
@@ -1,8 +1,10 @@
 package kz.mirazh.rest_service.domain;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class SimpleJson {
     private String name;
     private int age;
